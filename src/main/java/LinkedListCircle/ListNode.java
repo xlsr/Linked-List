18f8edc48889611e47b636e57f1e9c9b862f11ef
Roman Strelchenko <xlsr@cis-kiev.com>
2018-07-06 17:02:20 +0300

    final version

--- src/main/java/LinkedListCircle/ListNode.java
+++ src/main/java/LinkedListCircle/ListNode.java
@@ -1,10 +1,12 @@
 package LinkedListCircle;
 
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
         val = x;
         next = null;
     }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,19 @@
 import DesignLinkedList.MyLinkedList;
+import LinkedListCircle.ListNode;
+import LinkedListCircle.Solution;
 
 public class Main {
     public static void main(String[] args ) {
         try {
-            MyLinkedList myLinkedList = new MyLinkedList();
-            System.out.print("Get 0 "+myLinkedList.get(0)+"\n");
-            myLinkedList.addAtHead(1);
-            System.out.print("Get 0 "+myLinkedList.get(0)+"\n");
-            myLinkedList.addAtHead(2);
-            System.out.print("Get 0 "+myLinkedList.get(0)+"\n");
-            myLinkedList.addAtTail(3);
-            System.out.print("Get 2 "+myLinkedList.get(2)+"\n");
+
+            Solution solution = new Solution();
+            ListNode head = new ListNode(1);
+            //ListNode node = new ListNode(2);
+            //head.next = node;
+            //node.next = new ListNode(3);
+            //node.next.next = new ListNode(4);
+            System.out.println(solution.hasCycle(head));
+
         }catch (Exception e){
             System.out.print("Error:"+e.getMessage());
         }
