2ecc0132ed25644e3e9b449d0835102917029b05
Roman Strelchenko <xlsr@cis-kiev.com>
2018-07-06 17:15:00 +0300

    Linked List Circle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ import LinkedListCircle.Solution;
 public class Main {
     public static void main(String[] args ) {
         try {
+            ////////////////
+            System.out.println("Design Linked List");
             MyLinkedList linkedList = new MyLinkedList();
             linkedList.addAtHead(1);
             linkedList.addAtTail(3);
@@ -12,7 +14,9 @@ public class Main {
             linkedList.get(1);            // returns 2
             linkedList.deleteAtIndex(1);  // now the linked list is 1->3
             linkedList.get(1);            // returns 3
-
+            ///////////
+            System.out.println("Linked List Cycle");
+            //Given a linked list, determine if it has a cycle in it.
             Solution solution = new Solution();
             ListNode head = new ListNode(1);
             //ListNode node = new ListNode(2);
